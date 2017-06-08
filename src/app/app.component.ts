fc3900da51538ec45c3803a23968558f9e4fd561
dalo02 <daniel.lorincz@betssongroup.com>
2017-06-08 15:22:53 +0200

    Custom theme added a configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Todo } from "app/todo";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  isDarkTheme: boolean = false;
   newTodo: Todo = new Todo();
   constructor(@Inject("list") private list){}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { Footer } from './footer/footer.component';
 import { PointsListComponent } from './points-list/points-list.component';
 import { TodoListService } from "./services/todo-list.service";
 import { FormsModule } from "@angular/forms";
-import {MaterialModule } from '@angular/material';
+import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import 'hammerjs';
 
